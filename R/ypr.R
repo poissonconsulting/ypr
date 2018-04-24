7f8af92135750e276309a7d5932e17de04782c9f
Joe Thorley <joe@poissonconsulting.ca>
2018-04-24 10:32:56 -0700

    skeleton

---
+++ R/ypr.R
@@ -0,0 +1,16 @@
+#' Yield-Per-Recruit
+#'
+#' The default values are for large piscivorous Rainbow Trout.
+#'
+#' @param k A number of the growth coefficient.
+#' @param Linf A number of the mean maximum length.
+#' @param t0 A number
+#' @param alpha A number of the
+#' @param beta A number of the
+#' @param Lm A number of the length at maturity.
+#' @param Lv A number of the length at vulnerable to capture.
+#'
+#' @export
+ypr <- function(k, Linf = 1000, t0 = 0, alpha = 1, beta = 3, Lm = 500, Lv = 400) {
+
+}
