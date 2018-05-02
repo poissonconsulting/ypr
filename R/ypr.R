e40286070f99d3f65edd418dd4b9f1cc12afd614
Joe Thorley <joe@poissonconsulting.ca>
2018-05-01 17:23:35 -0700

    stuff

--- R/ypr.R
+++ R/ypr.R
@@ -4,12 +4,14 @@
 #'
 #' @param k A number of the growth coefficient.
 #' @param Linf A number of the mean maximum length.
-#' @param t0 A number
-#' @param alpha A number of the
-#' @param beta A number of the
-#' @param Lm A number of the length at maturity.
-#' @param Lv A number of the length at vulnerable to capture.
-#'
+#' @param t0 A number of the time at zero length.
+#' @param alpha A number of the length weight coefficient
+#' @param beta A number of the length weight power term.
+#' @param Lm A number of the length when mature.
+#' @param Lv A number of the length when vulnerable to capture.
+#' # need fecundity
+#' # M natural mortality
+#' # F fishing mortality
 #' @export
 ypr <- function(k, Linf = 1000, t0 = 0, alpha = 1, beta = 3, Lm = 500, Lv = 400) {
 
