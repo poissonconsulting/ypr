f767113fe7ff48eff4ebf21146c35a372b9cacef
Joe Thorley <joe@poissonconsulting.ca>
2018-05-16 10:07:03 -0700

    Removed a and fa as just scaling terms.

--- tests/testthat/test-fecundity.R
+++
@@ -1,6 +0,0 @@
-context("fecundity")
-
-test_that("fecundity", {
-  expect_identical(fecundity(c(0,49,50,100), Lm = 50, fa = 1, a = 1e-05, b = 3, fb = 1),
-                   c(0, 0, 1.25, 10))
-})
--- tests/testthat/test-weight.R
+++
@@ -1,5 +0,0 @@
-context("weight")
-
-test_that("weight", {
-  expect_identical(weight(c(0,10,100), 1e-05, 3), c(0, 0.01, 10))
-})
