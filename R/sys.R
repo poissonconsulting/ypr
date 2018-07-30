3ab2154a7be38a1ffd7fa760a414108924a4d2f9
Joe Thorley <joe@poissonconsulting.ca>
2018-07-30 15:44:28 -0700

    check Lm with Linf

--- R/sys.R
+++ R/sys.R
@@ -1,5 +1,6 @@
 .tmax <- 100L
 .Lmax <- 1e+04
+
 .parameters <- c("tmax", "k", "Linf", "t0", "Wb", "Lm", "fb", "Rt",
                 "S", "Lv", "Vp", "Llo", "Lup", "Nc", "mu", "rho", "eta", "Rk",
                 "R0", "Wa", "fa")
