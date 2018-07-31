fcac8a308282f4b3a7272531cb452ea53f5a097e
Joe Thorley <joe@poissonconsulting.ca>
2018-07-31 09:56:16 -0700

    add units

--- R/population.R
+++ R/population.R
@@ -4,31 +4,31 @@
 #' for a fish population.
 #'
 #' The default parameters are for a hypothetical population that
-#' isometrically grows to a mean maximum length of 100 with a k of 0.15,
+#' isometrically grows to a mean maximum length of 100 cm with a k of 0.15,
 #' and both matures and becomes vulnerable to harvest at a length of 50.
 #'
-#' @param tmax The maximum age.
-#' @param k The VB growth coefficient.
-#' @param Linf The VB mean maximum length.
-#' @param t0 The (theoretical) age at zero length.
+#' @param tmax The maximum age (yr).
+#' @param k The VB growth coefficient (per yr).
+#' @param Linf The VB mean maximum length (cm).
+#' @param t0 The (theoretical) age at zero length (yr).
 #' @param Wb The weight (as a function of length) scaling exponent.
-#' @param Lm The length at maturity.
+#' @param Lm The length at maturity (cm).
 #' @param fb The fecundity (as a function of weight) scaling exponent.
-#' @param tR The age from which survival is density-independent.
-#' @param M The instantaneous mortality rate.
+#' @param tR The age from which survival is density-independent (yr).
+#' @param M The instantaneous mortality rate (per yr).
 #' @param Mb The instantaneous mortality rate (as a function of length) scaling exponent.
-#' @param Lv The length at which 50\% vulnerable to harvest.
+#' @param Lv The length at which 50\% vulnerable to harvest (cm).
 #' @param Vp The vulnerability to harvest (as function of length) power.
-#' @param Llo The lower harvest slot length.
-#' @param Lup The upper harvest slot length.
+#' @param Llo The lower harvest slot length (cm).
+#' @param Lup The upper harvest slot length (cm).
 #' @param Nc The slot limits non-compliance probability.
 #' @param pi The annual capture probability.
 #' @param rho The release probability.
 #' @param Hm The hooking mortality.
 #' @param Rk The numbers of spawners per spawner at low density.
-#' @param R0 The number of recruits at the carrying capacity.
-#' @param Wa The (extrapolated) weight of a unit length individual.
-#' @param fa The (theoretical) fecundity of a unit weight female.
+#' @param R0 The number of recruits at the carrying capacity (ind).
+#' @param Wa The (extrapolated) weight of a 1 cm individual (g).
+#' @param fa The (theoretical) fecundity of a 1 g female (eggs).
 #' @return An object of class \code{ypr_population}.
 #' @seealso \code{\link{plot.ypr_population}} and \code{\link{ypr_population_update}}.
 #' @export
