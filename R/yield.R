67e0f9f84e69ee8f240f1bd480b3a7e78b9c4509
Joe Thorley <joe@poissonconsulting.ca>
2019-03-04 17:26:41 -0800

    ypr_yield.ypr_population

--- R/yield.R
+++ R/yield.R
@@ -1,3 +1,24 @@
+#' Yield
+#'
+#' Calculates the yield for a population.
+#'
+#' By default, with \code{Rmax = 1} the number of individuals is the proportion of the recruits at the carrying capacity.
+#' If the yield is given in terms of the biomass (kg) then the scaling also depends on the value of \code{Wa} (g).
+#'
+#' @inheritParams ypr_schedule
+#' @param Ly The minimum length (trophy) fish to consider when calculating the yield.
+#' @param harvest A flag indicating whether to calculate the yield for harvested fish or captures.
+#' @param biomass A flag indicating whether to calculate the yield in terms of the biomass versus number of individuals.
+#' @param ... Unused
+#' @return The yield as number of fish or biomass.
+#' @seealso \code{\link{ypr_population}} and \code{\link{ypr_optimize}}
+#' @export
+#' @examples
+#' ypr_yield(ypr_population())
+ypr_yield <- function(population, Ly = 0, harvest = TRUE, biomass = FALSE, ...) {
+  UseMethod("ypr_yield")
+}
+
 yield <- function(schedule, population, Ly = 0, harvest = TRUE, biomass = FALSE) {
   schedule <- as.list(schedule)
 
@@ -38,23 +59,9 @@ yield_pi <- function(pi, population, Ly, harvest, biomass) {
   yield(schedule, population, Ly = Ly, harvest = harvest, biomass = biomass)
 }
 
-#' Yield
-#'
-#' Calculates the yield for a population.
-#'
-#' By default, with \code{Rmax = 1} the number of individuals is the proportion of the recruits at the carrying capacity.
-#' If the yield is given in terms of the biomass (kg) then the scaling also depends on the value of \code{Wa} (g).
-#'
-#' @inheritParams ypr_schedule
-#' @param Ly The minimum length (trophy) fish to consider when calculating the yield.
-#' @param harvest A flag indicating whether to calculate the yield for harvested fish or captures.
-#' @param biomass A flag indicating whether to calculate the yield in terms of the biomass versus number of individuals.
-#' @return The yield as number of fish or biomass.
-#' @seealso \code{\link{ypr_population}} and \code{\link{ypr_optimize}}
+#' @describeIn ypr_yield  Yield Population
 #' @export
-#' @examples
-#' ypr_yield(ypr_population())
-ypr_yield <- function(population, Ly = 0, harvest = TRUE, biomass = FALSE) {
+ypr_yield.ypr_population <- function(population, Ly = 0, harvest = TRUE, biomass = FALSE, ...) {
   check_yield_parameters(population, Ly, harvest, biomass)
 
   schedule <- ypr_schedule(population)
