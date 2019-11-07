3bbe556c2e12ce62678aa1ebf3089ca84c192d33
joethorley <joe@poissonconsulting.ca>
2019-11-07 13:41:35 -0800

    all of population params to params

--- R/params.R
+++ R/params.R
@@ -1,4 +1,32 @@
 #' Parameter Descriptions for chk Functions
+#' @param tmax The maximum age (yr).
+#' @param k The VB growth coefficient (per yr).
+#' @param Linf The VB mean maximum length (cm).
+#' @param t0 The (theoretical) age at zero length (yr).
+#' @param Wb The weight (as a function of length) scaling exponent.
+#' @param Ls The length at which 50\% mature (cm).
+#' @param Sp The maturity (as a function of length) power.
+#' @param es The annual probability of a mature fish spawning.
+#' @param Sm The spawning mortality probability.
+#' @param fb The fecundity (as a function of weight) scaling exponent.
+#' @param tR The age from which survival is density-independent (yr).
+#' @param BH Recruitment follows a Beverton-Holt (1) or Ricker (0) relationship.
+#' @param Rk The lifetime spawners per spawner at low density.
+#' @param M The instantaneous mortality rate (per yr).
+#' @param Mb The instantaneous mortality rate (as a function of length) scaling exponent.
+#' @param Lv The length at which 50\% vulnerable to harvest (cm).
+#' @param Vp The vulnerability to harvest (as a function of length) power.
+#' @param Llo The lower harvest slot length (cm).
+#' @param Lup The upper harvest slot length (cm).
+#' @param Nc The slot limits non-compliance probability.
+#' @param pi The annual capture probability.
+#' @param rho The release probability.
+#' @param Hm The hooking mortality probability.
+#' @param Rmax The number of recruits at the carrying capacity (ind).
+#' @param A0 The initial post age tR density independent mortality probability.
+#' @param Wa The (extrapolated) weight of a 1 cm individual (g).
+#' @param fa The (theoretical) fecundity of a 1 g female (eggs).
+#' @param q The catchability (annual probability of capture) for a unit of effort.
 #' @param all A flag indicating whether to include all parameter values.
 #' @keywords internal
 #' @name params
