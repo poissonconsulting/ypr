e48ad67a7f716ce7389c9cc4926d1de24b715312
joethorley <joe@poissonconsulting.ca>
2019-11-07 17:00:16 -0800

    test plot

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,6 +1,12 @@
 context("plot")
 
 test_that("ypr_plot_schedule", {
+  pdf(tempfile())
+  teardown(dev.off())
+  expect_silent(plot(ypr_population()))
+})
+
+test_that("ypr_plot_schedule", {
   gp <- ypr_plot_schedule(ypr_population())
   expect_is(gp, "gg")
 })
