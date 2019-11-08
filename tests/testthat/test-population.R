80108cef710f40ad03a5655c1d2cf440059f7eb2
joethorley <joe@poissonconsulting.ca>
2019-11-07 16:56:22 -0800

    class chk_error

--- tests/testthat/test-population.R
+++ tests/testthat/test-population.R
@@ -6,7 +6,7 @@ test_that("population", {
   population2 <- ypr_population()
   population2$Rk <- 2.5
   expect_identical(ypr_population_update(population, Rk = 2.5), population2)
-  expect_error(ypr_population_update(population, Rk = 0.5), "^`Rk` must be between 1 and 100, not 0.5[.]$")
+  expect_error(ypr_population_update(population, Rk = 0.5), "^`Rk` must be between 1 and 100, not 0.5[.]$", class = "chk_error")
   expect_is(ypr_population_update(population, pi = 0.23456), "ypr_population")
 
   ypr_population_update(population, M = 0.2, Mb = -0.75)
