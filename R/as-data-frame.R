1e0b5a7aba4cf6c0ba5f4e0ba185752d48afba4b
joethorley <joe@poissonconsulting.ca>
2019-11-12 15:56:31 -0800

    as data frame returns tibble

--- R/as-data-frame.R
+++ R/as-data-frame.R
@@ -1,11 +1,11 @@
 #' @export
 as.data.frame.ypr_population <- function(x, ...) {
   x <- unclass(x)
-  as.data.frame(x)
+  as_tibble(as.data.frame(x))
 }
 
 #' @export
 as.data.frame.ypr_populations <- function(x, ...) {
   x <- lapply(x, as.data.frame)
-  do.call("rbind", x)
+  as_tibble(do.call("rbind", x))
 }
--- tests/testthat/test-as-data-frame.R
+++ tests/testthat/test-as-data-frame.R
@@ -3,11 +3,11 @@ context("as-data-frame")
 test_that("as.data.frame", {
   expect_identical(as.data.frame(ypr_population()),
                    structure(list(tmax = 20L, k = 0.15, Linf = 100, t0 = 0, k2 = 0.15,
-                                  Linf2 = 100, L2 = 1000, Wb = 3, Ls = 50, Sp = 100, es = 1,
-                                  Sm = 0, fb = 1, tR = 1L, BH = 1L, Rk = 3, n = 0.2, nL = 0.2,
-                                  Ln = 50, Lv = 50, Vp = 100, Llo = 0, Lup = 1000, Nc = 0,
-                                  pi = 0.2, rho = 0, Hm = 0, Rmax = 1, Wa = 0.01, fa = 1, q = 0.1), class = "data.frame", row.names = c(NA,
-                                                                                                                                        -1L)))
+    Linf2 = 100, L2 = 1000, Wb = 3, Ls = 50, Sp = 100, es = 1,
+    Sm = 0, fb = 1, tR = 1L, BH = 1L, Rk = 3, n = 0.2, nL = 0.2,
+    Ln = 50, Lv = 50, Vp = 100, Llo = 0, Lup = 1000, Nc = 0,
+    pi = 0.2, rho = 0, Hm = 0, Rmax = 1, Wa = 0.01, fa = 1, q = 0.1), class = c("tbl_df",
+"tbl", "data.frame"), row.names = c(NA, -1L)))
 
   expect_identical(as.data.frame(ypr_populations(Rk = c(3,4))),
                    structure(list(tmax = c(20L, 20L), k = c(0.15, 0.15), Linf = c(100,
@@ -19,5 +19,5 @@ test_that("as.data.frame", {
     0), Lup = c(1000, 1000), Nc = c(0, 0), pi = c(0.2, 0.2),
     rho = c(0, 0), Hm = c(0, 0), Rmax = c(1, 1), Wa = c(0.01,
     0.01), fa = c(1, 1), q = c(0.1, 0.1)), row.names = c("Rk_3",
-"Rk_4"), class = "data.frame"))
+"Rk_4"), class = c("tbl_df", "tbl", "data.frame")))
 })
