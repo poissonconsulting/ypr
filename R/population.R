fc4320252763a04308d7e71f910a66b56297ea04
joethorley <joe@poissonconsulting.ca>
2019-11-13 11:25:42 -0800

    remove default values defined by other and increase Ln to 1000

--- R/population.R
+++ R/population.R
@@ -10,11 +10,11 @@
 #' @examples
 #' ypr_population(k = 0.1, Linf = 90)
 ypr_population <- function(tmax = 20L, k = 0.15, Linf = 100, t0 = 0,
-                           k2 = k, Linf2 = Linf, L2 = 1000,
+                           k2 = 0.15, Linf2 = 100, L2 = 1000,
                            Wb = 3, Ls = 50, Sp = 100, es = 1, Sm = 0,
                            fb = 1,
-                           tR = 1L, BH = 1L, Rk = 3, n = 0.2, nL = n,
-                           Ln = 50,
+                           tR = 1L, BH = 1L, Rk = 3, n = 0.2, nL = 0.2,
+                           Ln = 1000,
                            Lv = 50, Vp = 100,
                            Llo = 0, Lup = 1000, Nc = 0,
                            pi = 0.2, rho = 0, Hm = 0,
--- tests/testthat/test-population.R
+++ tests/testthat/test-population.R
@@ -21,7 +21,7 @@ test_that("populations", {
                    structure(list(Popn_1 = structure(list(tmax = 20L, k = 0.15,
                                                           Linf = 100, t0 = 0, k2 = 0.15, Linf2 = 100, L2 = 1000, Wb = 3,
                                                           Ls = 50, Sp = 100, es = 1, Sm = 0, fb = 1, tR = 1L, BH = 1L,
-                                                          Rk = 2, n = 0.2, nL = 0.2, Ln = 50, Lv = 50, Vp = 100, Llo = 0,
+                                                          Rk = 2, n = 0.2, nL = 0.2, Ln = 1000, Lv = 50, Vp = 100, Llo = 0,
                                                           Lup = 1000, Nc = 0, pi = 0.2, rho = 0, Hm = 0, Rmax = 1,
                                                           Wa = 0.01, fa = 1, q = 0.1), class = "ypr_population")), class = "ypr_populations"))
 
@@ -78,12 +78,12 @@ test_that("ypr_populations_update", {
   structure(list(Popn_1 = structure(list(tmax = 20L, k = 0.15,
     Linf = 100, t0 = 0, k2 = 0.15, Linf2 = 100, L2 = 1000, Wb = 3,
     Ls = 50, Sp = 100, es = 1, Sm = 0, fb = 1, tR = 1L, BH = 1L,
-    Rk = 2.5, n = 0.2, nL = 0.2, Ln = 50, Lv = 50, Vp = 100,
+    Rk = 2.5, n = 0.2, nL = 0.2, Ln = 1000, Lv = 50, Vp = 100,
     Llo = 0, Lup = 1000, Nc = 0, pi = 0.2, rho = 0, Hm = 0, Rmax = 1,
     Wa = 0.01, fa = 1, q = 0.1), class = "ypr_population"), Popn_2 = structure(list(
     tmax = 20L, k = 0.15, Linf = 100, t0 = 0, k2 = 0.15, Linf2 = 100,
     L2 = 1000, Wb = 3, Ls = 50, Sp = 100, es = 1, Sm = 0, fb = 1,
-    tR = 1L, BH = 1L, Rk = 2.5, n = 0.2, nL = 0.2, Ln = 50, Lv = 50,
+    tR = 1L, BH = 1L, Rk = 2.5, n = 0.2, nL = 0.2, Ln = 1000, Lv = 50,
     Vp = 100, Llo = 0, Lup = 1000, Nc = 0, pi = 0.2, rho = 0,
     Hm = 0, Rmax = 1, Wa = 0.01, fa = 1, q = 0.1), class = "ypr_population")), class = "ypr_populations"))
 })
