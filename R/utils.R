22037b8e7aeb951a3cb3d9b8659bbfd9d501bbf3
joethorley <joe@poissonconsulting.ca>
2020-01-09 09:25:17 -0800

    fix biphasic growth when t0 != 0

--- R/utils.R
+++ R/utils.R
@@ -10,7 +10,7 @@ length_at_age <- function(population, age) {
   with(population, {
     if (L2 < 0) L2 <- Linf * (1 - exp(-k * (-L2 - t0)))
     L <- Linf * (1 - exp(-k * (age - t0)))
-    t2 <- -log(1 - min(L2 / Linf, 1)) / k - t0
+    t2 <- -log(1 - min(L2 / Linf, 1)) / k + t0
     L[age > t2] <- L2 + (Linf2 - L2) * (1 - exp(-k2 * (age[age > t2] - t2)))
     L[L < 0] <- 0
     L
