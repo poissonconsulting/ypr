0dcb5eacc4fd0e2f072f2d91c5479ce519980f54
joethorley <joe@poissonconsulting.ca>
2020-01-13 10:58:45 -0800

    ypr_schedule test

--- tests/testthat/test-aaa-deprecated.R
+++ tests/testthat/test-aaa-deprecated.R
@@ -1,8 +1,8 @@
 context("deprecated")
 
 test_that("ypr_schedule", {
-  expect_warning(
+  expect_identical(
     ypr_schedule(ypr_population()),
-    "`ypr_schedule[(][)]` is deprecated as of"
+    ypr_tabulate_schedule(ypr_population())
   )
 })
