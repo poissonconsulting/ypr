7b5ceb2d2bb29588ea8169a304d5111ea3db7626
Ayla Pearson <ayla@poissonconsulting.ca>
2021-09-02 16:18:58 -0700

    switching report template to be usethis instead of pasteing it together

--- R/report.R
+++ R/report.R
@@ -15,77 +15,6 @@ lines_population <- function(population) {
   paste0("ypr_population(", population, ")", collapse = "")
 }
 
-lines_head <- function(population, title, description, date) {
-  paste0('---
-title: "', title, '"
-date: "', date, '"
-output: html_document
----
-
-```{r setup, include = FALSE}
-knitr::opts_chunk$set(
-  echo = FALSE,
-  fig.width = 4,
-  fig.height = 4
-)
-```
-
-', description, "
-
-```{r}
-library(ypr)
-population <- ", lines_population(population), "
-```")
-}
-
-lines_body <- function(Ly, harvest, biomass) {
-  p0(
-    '```{r}
-knitr::kable(ypr_tabulate_parameters(population))
-```
-
-```{r}
-ypr_plot_schedule(population)
-ypr_plot_schedule(population, "Length", "Weight")
-ypr_plot_schedule(population, "Weight", "Fecundity")
-ypr_plot_schedule(population, "Length", "Spawning")
-```
-
-```{r}
-ypr_plot_schedule(population, "Length", "Vulnerability")
-ypr_plot_schedule(population, "Length", "Retention")
-ypr_plot_schedule(population, "Length", "FishingMortality")
-ypr_plot_schedule(population, "Length", "NaturalMortality")
-```
-
-```{r}
-ypr_plot_fish(population, color = "white")
-ypr_plot_fish(population, "Length", "Caught")
-ypr_plot_fish(population, "Age", "Spawners", color = "white")
-ypr_plot_fish(population, "Length", "Spawners")
-ypr_plot_biomass(population, color = "white")
-ypr_plot_biomass(population, y = "Eggs", color = "white")
-```
-
-```{r, fig.width = 6, fig.height = 4}
-ypr_plot_sr(population)
-knitr::kable(ypr_sr(population))
-knitr::kable(ypr_tabulate_sr(population))
-```
-
-```{r, fig.width = 6, fig.height = 4}
-ypr_plot_yield(population, Ly = ',
-    Ly, ", harvest = ",
-    harvest, ", biomass = ",
-    biomass, ")
-knitr::kable(ypr_tabulate_yield(population, Ly = ",
-    Ly, ", harvest = ",
-    harvest, ", biomass = ",
-    biomass, "))
-```"
-  )
-}
-
 #' Report
 #'
 #' Creates an Rmd file that can be used to generate a report.
@@ -95,7 +24,9 @@ knitr::kable(ypr_tabulate_yield(population, Ly = ",
 #' @family tabulate
 #' @export
 #' @examples
+#' \dontrun{
 #' ypr_report(ypr_population(), file = tempfile(), ask = FALSE)
+#' }
 ypr_report <- function(population,
                        Ly = 0,
                        harvest = TRUE,
@@ -103,7 +34,9 @@ ypr_report <- function(population,
                        title = "Population Report",
                        description = "",
                        date = Sys.Date(),
-                       file = "report", view = FALSE, ask = TRUE) {
+                       file = "report",
+                       view = FALSE,
+                       ask = TRUE) {
   chk_population(population)
   chk_number(Ly)
   chk_gte(Ly)
@@ -115,6 +48,7 @@ ypr_report <- function(population,
   chk_string(file)
   chk_flag(view)
   chk_flag(ask)
+  if (!requireNamespace("usethis")) err("Package 'usethis' must be installed.")
 
   if (grepl("[.](R|r)md$", file)) {
     wrn("File extension on argument `file` is deprecated (please remove).")
@@ -126,32 +60,37 @@ ypr_report <- function(population,
     return(invisible(character(0)))
   }
 
-  file.create(file)
-  con <- file(file, "w")
-  writeLines(lines_head(
-    population = population, title = title,
-    description = description, date = date
-  ),
-  con = con
+  data <- list(title = title,
+               date = date,
+               description = description,
+               population = lines_population(population),
+               Ly = Ly,
+               harvest = harvest,
+               biomass = biomass
+               )
+
+  usethis::use_template(
+    template = "template-report.Rmd",
+    save_as = file,
+    data = data,
+    package = "ypr"
   )
-  writeLines(lines_body(
-    Ly = Ly,
-    harvest = harvest,
-    biomass = biomass
-  ), con = con)
-  close(con)
+
   if (view) {
     if (!requireNamespace("rmarkdown")) {
       err("Package 'rmarkdown' is required to render the report to html.")
     }
+
     file_html <- p0(sub("[.](R|r)md$", "", file), ".html")
     if (!ask_file(file_html, ask)) {
       return(invisible(readLines(file)))
     }
+
     rmarkdown::render(file, output_format = "html_document", quiet = TRUE)
     if (!requireNamespace("rstudioapi")) {
       err("Package 'rstudioapi' is required to view the html report.")
     }
+
     rstudioapi::viewer(file_html)
   }
   invisible(readLines(file))
