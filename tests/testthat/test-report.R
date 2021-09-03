38f656e15928d5baa48c7a6eb16d07013b67839f
Ayla Pearson <ayla@poissonconsulting.ca>
2021-09-03 13:03:23 -0700

    lint and suppressing messages that display in the tests

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -2,20 +2,41 @@ test_that("report", {
   expect_identical(integer_parameters(), c("tmax", "tR", "BH"))
   # used to create temp package for testing
   suppressMessages(create_local_package())
-  report <- ypr_report(ypr_population(), ask = FALSE)
+  report <- suppressMessages(ypr_report(ypr_population(), ask = FALSE))
   expect_type(report, "character")
   expect_identical(report[1], "---")
   expect_identical(report[2], "title: \"Population Report\"")
-  expect_identical(report[55], "ypr_plot_yield(population, Ly = 0, harvest = TRUE, biomass = FALSE)")
-  expect_identical(report[56], "knitr::kable(ypr_tabulate_yield(population, Ly = 0, harvest = TRUE, biomass = FALSE))")
+  expect_identical(
+    report[55],
+    "ypr_plot_yield(population, Ly = 0, harvest = TRUE, biomass = FALSE)"
+  )
+  expect_identical(
+    report[56],
+    "knitr::kable(ypr_tabulate_yield(population, Ly = 0, harvest = TRUE, biomass = FALSE))"
+  )
 })
 
 test_that("report setting yield parameters", {
   expect_identical(integer_parameters(), c("tmax", "tR", "BH"))
   # used to create temp package for testing
   suppressMessages(create_local_package())
-  report <- ypr_report(ypr_population(), Ly = 10, harvest = FALSE, biomass = TRUE, file = "file1", ask = FALSE)
+  report <- suppressMessages(
+    ypr_report(
+      ypr_population(),
+      Ly = 10,
+      harvest = FALSE,
+      biomass = TRUE,
+      file = "file1",
+      ask = FALSE
+    )
+  )
   expect_type(report, "character")
-  expect_identical(report[55], "ypr_plot_yield(population, Ly = 10, harvest = FALSE, biomass = TRUE)")
-  expect_identical(report[56], "knitr::kable(ypr_tabulate_yield(population, Ly = 10, harvest = FALSE, biomass = TRUE))")
+  expect_identical(
+    report[55],
+    "ypr_plot_yield(population, Ly = 10, harvest = FALSE, biomass = TRUE)"
+  )
+  expect_identical(
+    report[56],
+    "knitr::kable(ypr_tabulate_yield(population, Ly = 10, harvest = FALSE, biomass = TRUE))"
+  )
 })
