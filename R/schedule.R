fc6e12b869207003ad9f74ece89d218f29ae5098
Ayla Pearson <ayla@poissonconsulting.ca>
2021-10-04 16:28:42 -0700

    framing up new ecotype method for tabulate_schedule

--- R/schedule.R
+++ R/schedule.R
@@ -19,6 +19,36 @@ ypr_tabulate_schedule.ypr_population <- function(object) {
   population <- object
   chk_population(population)
 
+  schedule <- math(population)
+
+  as_tibble(schedule)
+}
+
+#' @describeIn ypr_tabulate_schedule Tabulate Schedule
+#' @export
+ypr_tabulate_schedule.ypr_ecotypes <- function(object) {
+
+  chk::chk_s3_class(object, "ypr_ecotypes")
+
+  weights <- attr(object, "weights")
+  eco_names <- names(object)
+
+  # need to loop and then add the weights and names
+  schedule <- math(population)
+
+  schedule <- as_tibble(schedule)
+
+
+
+  yield[["Ecotype"]] <- eco_names
+  yield[["Proportions"]] <- weights
+
+
+}
+
+
+math <- function(population) {
+
   schedule <- with(population, {
     t <- tR:tmax
     nt <- length(t)
@@ -27,14 +57,14 @@ ypr_tabulate_schedule.ypr_population <- function(object) {
     E <- fa * W^fb
     if (Ls < 0) Ls <- length_at_age(population, -Ls)
     S <- exp(log(L / 1000) * Sp) / (exp(log(Ls / 1000) * Sp) +
-      exp(log(L / 1000) * Sp)) * es
+                                      exp(log(L / 1000) * Sp)) * es
     N <- rep(n, nt)
     if (Ln < 0) Ln <- length_at_age(population, -Ln)
     N[L >= Ln] <- nL
     N <- 1 - ((1 - N) * (1 - S * Sm))
     if (Lv < 0) Lv <- length_at_age(population, -Lv)
     V <- exp(log(L / 1000) * Vp) / (exp(log(Lv / 1000) * Vp) +
-      exp(log(L / 1000) * Vp))
+                                      exp(log(L / 1000) * Vp))
     C <- pi * V
     R <- rep(1 - rho, nt)
     R[L < Llo | L > Lup] <- Nc
@@ -54,5 +84,10 @@ ypr_tabulate_schedule.ypr_population <- function(object) {
     )
   })
 
-  as_tibble(schedule)
 }
+
+
+
+
+
+
