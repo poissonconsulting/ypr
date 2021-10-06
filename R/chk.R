9b38c8731f80632bf24683bb6b9115c4af45096f
Ayla Pearson <ayla@poissonconsulting.ca>
2021-10-05 17:06:27 -0700

    switching order and adding x_name parameter to improve error message

--- R/chk.R
+++ R/chk.R
@@ -16,9 +16,9 @@ chk_populations <- function(x, x_name = NULL) {
 
   chk_list(x, x_name = x_name)
 
+  chk_s3_class(x, "ypr_populations", x_name)
   x_name <- paste("elements of", x_name)
 
-  chk_s3_class(x, "ypr_populations")
 
   chk_all(x, chk_population, x_name = x_name)
 
