0c33d25451c34195c655b467ddb6c166ed81bc96
Ayla Pearson <ayla@poissonconsulting.ca>
2021-10-06 16:45:42 -0700

    adding tests for dataframe conversion and printing

--- tests/testthat/test-base.R
+++ tests/testthat/test-base.R
@@ -80,3 +80,31 @@ test_that("update", {
     ypr_population(Rk = 5)
   )
 })
+
+test_that("data.frame conversion for ecotypes functions as expected", {
+  expect_identical(
+    as.data.frame(as_ypr_ecotypes()),
+    structure(list(
+      tmax = c(20L, 20L), k = c(0.15, 0.15), Linf = c(100, 1000), t0 = c(0, 0),
+      k2 = c(0.15, 0.15), Linf2 = c(100, 100), L2 = c(1000, 1000), Wb = c(3, 3),
+      Ls = c(50, 50), Sp = c(100, 100), es = c(1, 1), Sm = c(0, 0), fb = c(1, 1),
+      tR = c(1L, 1L), BH = c(1L, 1L), Rk = c(3, 3), n = c(0.2, 0.2),
+      nL = c(0.2, 0.2), Ln = c(1000, 1000), Lv = c(50, 50), Vp = c(100, 100),
+      Llo = c(0, 0), Lup = c(1000, 1000), Nc = c(0, 0), pi = c(0.2, 0.2),
+      rho = c(0, 0), Hm = c(0, 0), Rmax = c(1, 1), Wa = c(0.01, 0.01),
+      fa = c(1, 1), q = c(0.1, 0.1)
+      ),
+      row.names = c("Linf_100", "Linf_1000"),
+      proportions = c(0.5, 0.5),
+      class = c("tbl_df", "tbl", "data.frame")
+    )
+  )
+})
+
+test_that("print method for ecotypes outputs correctly", {
+
+  expect_identical(
+    capture_output(print(as_ypr_ecotypes())),
+    "tmax:  20\nk:     0.15\nLinf:  100, 1000\nt0:    0\nk2:    0.15\nLinf2: 100\nL2:    1000\nWb:    3\nLs:    50\nSp:    100\nes:    1\nSm:    0\nfb:    1\ntR:    1\nBH:    1\nRk:    3\nn:     0.2\nnL:    0.2\nLn:    1000\nLv:    50\nVp:    100\nLlo:   0\nLup:   1000\nNc:    0\npi:    0.2\nrho:   0\nHm:    0\nRmax:  1\nWa:    0.01\nfa:    1\nq:     0.1\nEcotype: Linf_100, Linf_1000\nProportion: 0.5, 0.5"
+  )
+})
