e4ff8f217d5a21a5f15a6eeb9d4dac6f1bbbd94a
Ayla Pearson <ayla@poissonconsulting.ca>
2021-10-06 11:20:30 -0700

    switching order of functions in scripts

--- R/population.R
+++ R/population.R
@@ -158,55 +158,6 @@ ypr_populations <- function(..., expand = TRUE) {
   populations
 }
 
-#' Population Names
-#'
-#' @inheritParams params
-#'
-#' @return A character vector of the unique population names.
-#' @family populations
-#' @export
-#' @examples
-#' ypr_population_names(ypr_populations(Rk = c(2.5, 3, 2.5), expand = FALSE))
-ypr_population_names <- function(populations) {
-  populations <- as.data.frame(populations)
-  populations <- populations[, vapply(
-    populations,
-    FUN = function(x) length(unique(x)) > 1,
-    TRUE
-  )]
-  if (!ncol(populations)) {
-    return(paste0("Popn_", seq_len(nrow(populations))))
-  }
-  populations <- as.list(populations)
-  populations <- purrr::map(
-    populations,
-    .sub,
-    pattern = "[.]",
-    replacement = "_"
-  )
-  populations <- purrr::map2(
-    populations,
-    names(populations),
-    function(x, y) paste(y, x, sep = "_")
-  )
-  populations <- purrr::transpose(populations)
-  populations <- purrr::map(
-    populations,
-    function(x) {
-      paste0(unname(unlist(x)),
-        collapse = "_"
-      )
-    }
-  )
-  names <- unlist(populations)
-  duplicates <- unique(names[duplicated(names)])
-  for (duplicate in duplicates) {
-    bol <- names == duplicate
-    names[bol] <- paste(names[bol], "Popn", 1:sum(bol), sep = "_")
-  }
-  names
-}
-
 #' Ecotypes
 #'
 #' @inheritParams params
@@ -266,4 +217,53 @@ ypr_ecotypes <- function(..., weights) {
   attr(ecotypes, "weights") <- weights
 
   ecotypes
+}
+
+#' Population Names
+#'
+#' @inheritParams params
+#'
+#' @return A character vector of the unique population names.
+#' @family populations
+#' @export
+#' @examples
+#' ypr_population_names(ypr_populations(Rk = c(2.5, 3, 2.5), expand = FALSE))
+ypr_population_names <- function(populations) {
+  populations <- as.data.frame(populations)
+  populations <- populations[, vapply(
+    populations,
+    FUN = function(x) length(unique(x)) > 1,
+    TRUE
+  )]
+  if (!ncol(populations)) {
+    return(paste0("Popn_", seq_len(nrow(populations))))
+  }
+  populations <- as.list(populations)
+  populations <- purrr::map(
+    populations,
+    .sub,
+    pattern = "[.]",
+    replacement = "_"
+  )
+  populations <- purrr::map2(
+    populations,
+    names(populations),
+    function(x, y) paste(y, x, sep = "_")
+  )
+  populations <- purrr::transpose(populations)
+  populations <- purrr::map(
+    populations,
+    function(x) {
+      paste0(unname(unlist(x)),
+        collapse = "_"
+      )
+    }
+  )
+  names <- unlist(populations)
+  duplicates <- unique(names[duplicated(names)])
+  for (duplicate in duplicates) {
+    bol <- names == duplicate
+    names[bol] <- paste(names[bol], "Popn", 1:sum(bol), sep = "_")
+  }
+  names
 }
