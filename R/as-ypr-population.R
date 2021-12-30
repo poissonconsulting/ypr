fffa2fa090b56c003fe629e1a999e38630aeca27
Joe Thorley <joe@poissonconsulting.ca>
2021-12-30 11:53:40 -0800

    - `as_ypr_population()` now S3 generic with methods for population, populations, ecotypes and data.frame.

--- R/as-ypr-population.R
+++ R/as-ypr-population.R
@@ -1,12 +1,59 @@
 #' Coerce to a Population Object
 #'
-#' Creates an object of class ypr_population from a data frame of population parameters.
+#' @param x The object to coerce.
+#' @param ... Additional arguments.
+#' @return An object of class ypr_ecotypes.
+#' @family population
+#' @export
+as_ypr_population <- function(x, ...) {
+  UseMethod("as_ypr_population")
+}
+
+#' @describeIn as_ypr_population Coerce a data.frame to an Population Object
 #'
-#' @inheritParams params
 #' @export
 #' @examples
 #' as_ypr_population(as.data.frame(ypr_population()))
-as_ypr_population <- function(x) {
+as_ypr_population.data.frame <- function(x, ...) {
   chk_data(x)
+  chk_unused(...)
+
   do.call("ypr_population", x)
+}
+
+#' @describeIn as_ypr_population Coerce a Population Object to an Population Object
+#'
+#' @export
+#' @examples
+#' as_ypr_population(ypr_populations())
+as_ypr_population.ypr_population <- function(x, ...) {
+  check_population(x)
+  chk_unused(...)
+  x
+}
+
+#' @describeIn as_ypr_population Coerce a Populations Object of length 1 to a Population Object
+#'
+#' @export
+#' @examples
+#' as_ypr_population(ypr_populations())
+as_ypr_population.ypr_populations <- function(x, ...) {
+  check_populations(x)
+  chk_unused(...)
+
+  check_dim(x, dim = length, values = 1L)
+  x[[1]]
+}
+
+#' @describeIn as_ypr_population Coerce a Ecotypes Object of length 1 to a Population Object
+#'
+#' @export
+#' @examples
+#' as_ypr_population(ypr_ecotypes())
+as_ypr_population.ypr_ecotypes <- function(x, ...) {
+  check_ecotypes(x)
+  chk_unused(...)
+
+  check_dim(x, dim = length, values = 1L)
+  x[[1]]
 }
--- tests/testthat/test-as-ypr-population.R
+++ tests/testthat/test-as-ypr-population.R
@@ -1,10 +1,37 @@
-test_that("as_ypr_population", {
+test_that("as_ypr_population data.frame", {
   expect_identical(
     as_ypr_population(
       as.data.frame(
-        ypr_population()
+        ypr_population(Linf = 150)
       )
     ),
-    ypr_population()
+    ypr_population(Linf = 150)
+  )
+})
+
+test_that("as_ypr_population population", {
+  expect_identical(
+    as_ypr_population(
+        ypr_population(Linf = 150)
+    ),
+    ypr_population(Linf = 150)
+  )
+})
+
+test_that("as_ypr_population populations", {
+  expect_identical(
+    as_ypr_population(
+      ypr_populations(Linf = 150)
+    ),
+    ypr_population(Linf = 150)
+  )
+})
+
+test_that("as_ypr_population ecotypes", {
+  expect_identical(
+    as_ypr_population(
+      ypr_ecotypes(Linf = 150)
+    ),
+    ypr_population(Linf = 150)
   )
 })
