955a9b68172209cf9dea5eda9686316c887b6081
Joe Thorley <joe@poissonconsulting.ca>
2022-01-05 10:35:55 -0800

    tabulate fish

--- tests/testthat/test-tabulate-fish.R
+++ tests/testthat/test-tabulate-fish.R
@@ -1,16 +1,11 @@
-test_that("ypr_tabulate_fish works", {
+test_that("ypr_tabulate_fish population", {
   fish <- ypr_tabulate_fish(ypr_population(n = ypr:::inst2inter(0.2)))
-  expect_identical(colnames(fish), c(
-    "Age", "Survivors", "Spawners", "Caught",
-    "Harvested", "Released", "HandlingMortalities"
-  ))
-  expect_identical(fish[[1]], as.double(1:20))
-  expect_equal(fish$Survivors[1:2], c(0.134, 0.110), tolerance = 0.01)
+  expect_s3_class(fish, "tbl_df")
+  expect_snapshot_data(fish, "n0_2")
+})
 
+test_that("ypr_tabulate_fish population Length", {
   fish <- ypr_tabulate_fish(ypr_population(), x = "Length")
-  expect_identical(colnames(fish), c(
-    "Length", "Survivors", "Spawners", "Caught",
-    "Harvested", "Released", "HandlingMortalities"
-  ))
-  expect_identical(fish$Length[1:2], c(14, 26))
+  expect_s3_class(fish, "tbl_df")
+  expect_snapshot_data(fish, "length")
 })
