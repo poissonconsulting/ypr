2619d46078bd67e0cb5bdbe894616a811b45304c
Joe Thorley <joe@poissonconsulting.ca>
2022-04-24 17:09:58 -0700

    test tabulate-sr snapshot

--- tests/testthat/test-tabulate-sr.R
+++ tests/testthat/test-tabulate-sr.R
@@ -1,48 +1,13 @@
 test_that("ypr_tabulate_sr population", {
   sr <- ypr_tabulate_sr(ypr_population())
 
-  expect_error(chk::check_data(
-    sr,
-    values = list(
-      Type = c("unfished", "actual", "optimal"),
-      pi = c(0, 1),
-      u = c(0, 1),
-      Eggs = c(0, .Machine$double.xmax),
-      Recruits = c(0, .Machine$double.xmax),
-      Spawners = c(0, .Machine$double.xmax),
-      Fecundity = c(0, .Machine$double.xmax)
-    ),
-    nrow = TRUE,
-    exclusive = TRUE,
-    order = TRUE
-  ), NA)
-
-  expect_identical(sr$Type, c("unfished", "actual", "optimal"))
+  expect_s3_class(sr, "tbl_df")
+  expect_snapshot_data(sr, "srpopulation")
 })
 
 test_that("ypr_tabulate_sr populations", {
   sr <- ypr_tabulate_sr(ypr_populations(Rk = c(3, 5)))
 
-  expect_error(chk::check_data(
-    sr,
-    values = list(
-      Type = c("unfished", "actual", "optimal"),
-      pi = c(0, 1),
-      u = c(0, 1),
-      Eggs = c(0, .Machine$double.xmax),
-      Recruits = c(0, .Machine$double.xmax),
-      Spawners = c(0, .Machine$double.xmax),
-      Fecundity = c(0, .Machine$double.xmax),
-      Rk = c(3, 3, 5)
-    ),
-    nrow = TRUE,
-    exclusive = TRUE,
-    order = TRUE
-  ), NA)
-
-  expect_identical(colnames(sr), c(
-    "Type", "pi", "u", "Eggs", "Recruits",
-    "Spawners", "Fecundity", "Rk"
-  ))
-  expect_identical(sr$Rk, c(3, 3, 3, 5, 5, 5))
+  expect_s3_class(sr, "tbl_df")
+  expect_snapshot_data(sr, "srpopulationRk")
 })
