af452914183c2bd6e8b1257a8009549c06a0cd80
Joe Thorley <joe@poissonconsulting.ca>
2022-05-01 14:51:24 -0700

    biomass and tabulate yield separate

--- R/tabulate-yield.R
+++ R/tabulate-yield.R
@@ -70,32 +70,6 @@ ypr_tabulate_parameters <- function(population) {
   as_tibble(parameters)
 }
 
-#' Tablulate Biomass (and Eggs)
-#'
-#' Produces a data frame of the 'Weight' and 'Fecundity' and the number of
-#' 'Survivors' and 'Spawners' and the total 'Biomass' and 'Eggs' by 'Age' class.
-#'
-#' @inheritParams params
-#' @return A data frame
-#' @family tabulate
-#' @family biomass
-#' @export
-#' @examples
-#' ypr_tabulate_biomass(ypr_population())
-ypr_tabulate_biomass <- function(population) {
-  schedule <- ypr_tabulate_schedule(population)
-  fish <- ypr_tabulate_fish(population)
-
-  schedule <- schedule[c("Age", "Length", "Weight", "Fecundity")]
-  fish <- fish[c("Survivors", "Spawners")]
-
-  biomass <- cbind(schedule, fish)
-  biomass$Biomass <- biomass$Weight * biomass$Survivors
-  biomass$Eggs <- (biomass$Fecundity * biomass$Spawners) / 2
-
-  as_tibble(biomass)
-}
-
 #' @describeIn ypr_tabulate_yield Tabulate Yield
 #' @export
 ypr_tabulate_yield.ypr_population <- function(object,
@@ -221,7 +195,6 @@ ypr_tabulate_yield.ypr_ecotypes <- function(object,
   if (!average) {
     return(yield)
   }
-  .NotYetImplemented()
   yield
 }
 
--- tests/testthat/test-tabulate-yield.R
+++ tests/testthat/test-tabulate-yield.R
@@ -119,24 +119,3 @@ test_that("ypr_tabulate_yield extinct population", {
   expect_equal(yield$pi, c(0.2, 0.0926725376181979))
   expect_equal(yield$Yield, c(0, 0.0857039391276462))
 })
-
-test_that("ypr_tabulate_biomass", {
-  biomass <- ypr_tabulate_biomass(ypr_population())
-
-  expect_error(chk::check_data(
-    biomass,
-    values = list(
-      Age = c(1L, 100L),
-      Length = c(0, .Machine$double.xmax),
-      Weight = c(0, .Machine$double.xmax),
-      Fecundity = c(0, .Machine$double.xmax),
-      Survivors = c(0, 1),
-      Spawners = c(0, .Machine$double.xmax),
-      Biomass = c(0, .Machine$double.xmax),
-      Eggs = c(0, .Machine$double.xmax)
-    ),
-    nrow = TRUE,
-    exclusive = TRUE,
-    order = TRUE
-  ), NA)
-})
